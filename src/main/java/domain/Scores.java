56a736c8775db35ffad5e9e36520c5f4c6a79868
RudiaMoon <impactyoung@gmail.com>
2019-10-19 11:37:30 +0900

    Add TEST 스코어 초과 예외처리

--- src/main/java/domain/Scores.java
+++ src/main/java/domain/Scores.java
@@ -6,16 +6,27 @@ import java.util.List;
 public class Scores {
 
     private final List<ScoreType> scores;
+    int resultNumber;
+    private int maxChance;
 
     public Scores(int chance) {
+        this.maxChance = chance;
         scores = new ArrayList<>(chance);
     }
 
     public void register(int score) {
-//        if (scores.size() > 2) {
-//            throw new ArrayIndexOutOfBoundsException();
-//        }
+        if (scores.size() > maxChance) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
 
+        if (scores.size() >= 2) {
+            for (ScoreType type : scores) {
+                resultNumber += type.getScore();
+            }
+        }
+        if (maxChance < 2 && resultNumber > 10) {
+            throw new IllegalArgumentException();
+        }
         scores.add(ScoreType.of(score));
     }
 
--- src/test/java/domain/ScoresTest.java
+++ src/test/java/domain/ScoresTest.java
@@ -6,35 +6,34 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoresTest {
-
-    @Test
-    public void 입력된_숫자만큼_점수기회를_갖는다(){
-        // given
-        int chance = 3;
-        Scores scores = new Scores(chance);
-
-        // when, then
-        assertThat(scores.getChanceNumber()).isEqualTo(chance);
-    }
-
     @Test
     public void 주어진_기회만큼_점수등록() {
-        int chance = 2;
-        Scores scores = new Scores(chance);
+        int maxChance = 2;
+        Scores scores = new Scores(maxChance);
 
         scores.register(5);
         scores.register(3);
 
-        assertThat(scores.getChanceNumber()).isEqualTo(chance);
+        assertThat(scores.getChanceNumber()).isEqualTo(maxChance);
     }
 
     @Test
     void 최대점수를_초과한경우_에러를_발생시킨다() {
-        int chance = 2;
-        Scores scores = new Scores(chance);
+        int maxChance = 2;
+        Scores scores = new Scores(maxChance);
 
         scores.register(5);
         scores.register(3);
-        Assertions.assertThrows(IllegalAccessException.class, () -> scores.register(1));
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> scores.register(1));
+    }
+
+    @Test
+    void 최대점수가_10을_초과하면_에러() {
+        int maxChance = 2;
+        Scores scores = new Scores(maxChance);
+
+        scores.register(5);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> scores.register(6));
     }
 }
