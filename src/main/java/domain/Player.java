40983cd02df424b9036041abc78ff3e2d38551da
Seoyeong-Kim <seoyeong2352@gmail.com>
2019-10-19 09:54:15 +0900

    feat : Player객체 구현

---
+++ src/main/java/domain/Player.java
@@ -0,0 +1,20 @@
+package domain;
+
+public class Player {
+
+    private static final int NAME_LENGTH = 3;
+    private final String name;
+
+    private Player(String name) {
+        this.name = name;
+    }
+
+    public static Player of(String name) {
+        if(name.length() != NAME_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+
+        return new Player(name);
+    }
+
+}
---
+++ src/test/java/domain/PlayerTest.java
@@ -0,0 +1,15 @@
+package domain;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PlayerTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"SS", "SSSS"})
+    public void 플레이어_이름은_3글자가_아니면_안된다(String name) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Player.of(name));
+    }
+}
